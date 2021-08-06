235188262d1be297a11b24566b7c19fd32855ec9
AugCode <79752571+augustina55@users.noreply.github.com>
2021-08-06 20:00:06 +0530

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [fullname] CHAR(20) NOT NULL, 
+    [username] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(50) NOT NULL, 
+    [question] VARCHAR(50) NOT NULL, 
+    [answer] VARCHAR(50) NOT NULL
+)
